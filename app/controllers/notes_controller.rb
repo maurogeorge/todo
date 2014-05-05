424b39a77d5caee1e97dc623beb81f6fe3ab7af6
Mauro George <maurogot@gmail.com>
2014-05-05 15:07:30 -0300

    Create notes#create

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,26 @@
 class NotesController < ApplicationController
 
   def index
-    @notes = Note.all
+    fetch_notes
   end
+
+  def create
+    @note = Note.new(notes_params)
+    if @note.save
+      redirect_to root_path, notice: 'Nota criada com sucesso!'
+    else
+      fetch_notes
+      render 'index'
+    end
+  end
+
+  private
+
+    def notes_params
+      params.require(:note).permit(:content)
+    end
+
+    def fetch_notes
+      @notes = Note.all
+    end
 end
--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -20,4 +20,69 @@ describe NotesController do
       expect(assigns(:notes)).to include(note)
     end
   end
+
+  describe "POST 'create'" do
+
+    context 'with valid params' do
+
+      let(:params) do
+        { note: attributes_for(:note) }
+      end
+
+      it 'creates a new note' do
+        expect do
+          post :create, params
+        end.to change(Note, :count).by(1)
+      end
+
+      it 'assigns a newly @note' do
+        post :create, params
+        expect(assigns(:note)).to be_a(Note)
+      end
+
+      it 'persists @note' do
+        post :create, params
+        expect(assigns(:note)).to be_persisted
+      end
+
+      it 'redirects to notes path' do
+        post :create, params
+        should redirect_to(root_path)
+      end
+
+      it 'sets the flash correctly' do
+        post :create, params
+        should set_the_flash[:notice].to(/sucesso/)
+      end
+    end
+
+    context 'with invalid params' do
+
+      let(:params) do
+        { note: attributes_for(:note, content: '') }
+      end
+
+      it 'does not create a new note' do
+        expect do
+          post :create, params
+        end.to_not change(Note, :count)
+      end
+
+      it 'assigns @note with a new one' do
+        post :create, params
+        expect(assigns(:note)).to be_a_new(Note)
+      end
+
+      it "re-renders the 'index' template" do
+        post :create, params
+        should render_template('index')
+      end
+
+      it 'assigns all notes' do
+        note = create(:note)
+        post :create, params
+        expect(assigns(:notes)).to include(note)
+      end
+    end
+  end
 end
