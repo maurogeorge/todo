c4917bf36058ea260ea70019a05195eae4332fab
Mauro George <maurogot@gmail.com>
2014-05-05 14:45:52 -0300

    Create route to notes#create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Todo::Application.routes.draw do
 
   root to: 'notes#index'
 
+  resources :notes, only: [:create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/routing/notes_routing_spec.rb
+++ spec/routing/notes_routing_spec.rb
@@ -7,12 +7,20 @@ describe NotesController do
     it 'routes to notes#index' do
       expect(get('/')).to route_to('notes#index')
     end
+
+    it 'routes to notes#create' do
+      expect(post('/notes')).to route_to('notes#create')
+    end
   end
 
   describe 'route helpers' do
 
     it 'root_path' do
       expect(root_path).to eq('/')
+    end
+
+    it 'notes_path' do
+      expect(notes_path).to eq('/notes')
     end
   end
 end
