fab6c80cf470239f84d1eb22c9114b3c8d2f1d73
Mauro George <maurogot@gmail.com>
2014-05-05 12:07:22 -0300

    Initial commit.
    Generated by pah version 0.0.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo::Application.config.secret_key_base = '82bd73ec3df60fd6b04c3e49805b92ccaa6685d593841e1960854fc8c84f1b77165c00d6ac7d9ecc0cbaae81a116bd8fd3bb300230b12eaf5988aaa8c671efbd'
